6f4b7433a95a7cc23cd585ec11195af7f80d3617
0mer <103336973+0merS@users.noreply.github.com>
2025-06-30 14:34:11 +0300

    Update blog.js

--- pages/kulup/blog.js
+++ pages/kulup/blog.js
@@ -1,11 +1,8 @@
 export default function Blog() {
   return (
-    <div style={{ padding: '20px' }}>
-      <h1>Blog</h1>
-      <div style={{ marginTop: '15px' }}>
-        <h3>İlk Roket Denememiz</h3>
-        <p>Yazar: Ahmet - 10 Eylül 2023</p>
-      </div>
+    <div style={{padding:'20px'}}>
+      <h2>Blog</h2>
+      <p>En son yazılar yakında eklenecek!</p>
     </div>
-  );
+  )
 }
