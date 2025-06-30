984be9fee2e2769f8df4bda67b7ccf5602607013
0mer <103336973+0merS@users.noreply.github.com>
2025-06-30 13:37:07 +0300

    Update takvim.js

--- pages/kulup/takvim.js
+++ pages/kulup/takvim.js
@@ -1,1 +1,19 @@
+export default function Takvim() {
+  const etkinlikler = [
+    { tarih: "10 Ekim 2023", etkinlik: "Açılış Toplantısı" },
+    { tarih: "25 Aralık 2023", etkinlik: "Yılbaşı Workshop" }
+  ];
 
+  return (
+    <div>
+      <h1>Etkinlik Takvimi</h1>
+      <ul>
+        {etkinlikler.map((item, index) => (
+          <li key={index}>
+            <strong>{item.tarih}:</strong> {item.etkinlik}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
