c7b84df0d5f090e47be21bec767e5492c3e80afb
0mer <103336973+0merS@users.noreply.github.com>
2025-06-30 13:33:08 +0300

    Update blog.js

--- pages/kulup/blog.js
+++ pages/kulup/blog.js
@@ -1,1 +1,20 @@
+export default function Blog() {
+  const yazilar = [
+    { id: 1, baslik: "Roket Yapımı 101", yazar: "Ahmet" },
+    { id: 2, baslik: "Drone Yarışları", yazar: "Mehmet" }
+  ];
 
+  return (
+    <div>
+      <h1>Blog Yazıları</h1>
+      <ul>
+        {yazilar.map((yazi) => (
+          <li key={yazi.id}>
+            <h3>{yazi.baslik}</h3>
+            <p>Yazar: {yazi.yazar}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
