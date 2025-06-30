8a928f0c36d3659865b04b144ad67e648404cba3
0mer <103336973+0merS@users.noreply.github.com>
2025-06-30 13:32:42 +0300

    Update index.js

--- pages/kulup/index.js
+++ pages/kulup/index.js
@@ -1,1 +1,17 @@
+import Link from 'next/link';
 
+export default function KulupAnasayfa() {
+  return (
+    <div>
+      <h1>Kulüp Sayfasına Hoşgeldiniz!</h1>
+      <nav>
+        <Link href="/kulup/faaliyetler">Faaliyetler</Link> | 
+        <Link href="/kulup/blog">Blog</Link> | 
+        <Link href="/kulup/takvim">Takvim</Link>
+      </nav>
+      
+      {/* Örnek içerik */}
+      <p>Bu haftaki drone atölyemize katılın!</p>
+    </div>
+  );
+}
