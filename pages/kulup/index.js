6410c96701894a1392538f3acba82c2d9608df55
0mer <103336973+0merS@users.noreply.github.com>
2025-06-30 13:34:38 +0300

    Update index.js

--- pages/kulup/index.js
+++ pages/kulup/index.js
@@ -1,17 +1,24 @@
 import Link from 'next/link';
 
-export default function KulupAnasayfa() {
+export default function Kulup() {
   return (
     <div>
-      <h1>Kulüp Sayfasına Hoşgeldiniz!</h1>
-      <nav>
-        <Link href="/kulup/faaliyetler">Faaliyetler</Link> | 
-        <Link href="/kulup/blog">Blog</Link> | 
-        <Link href="/kulup/takvim">Takvim</Link>
+      <h1>Havacılık ve Teknoloji Kulübü</h1>
+      <nav style={{ margin: '20px 0' }}>
+        <Link href="/kulup/faaliyetler" style={{ marginRight: '15px', color: 'blue' }}>
+          Faaliyetler
+        </Link>
+        <Link href="/kulup/blog" style={{ marginRight: '15px', color: 'blue' }}>
+          Blog
+        </Link>
+        <Link href="/kulup/takvim" style={{ color: 'blue' }}>
+          Takvim
+        </Link>
       </nav>
-      
-      {/* Örnek içerik */}
-      <p>Bu haftaki drone atölyemize katılın!</p>
+
+      <div>
+        <p>Kulübümüzün resmi web sayfasına hoşgeldiniz!</p>
+      </div>
     </div>
   );
 }
